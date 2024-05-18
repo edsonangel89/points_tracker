12a0b365bc0549f03adbadd92013ae4bb88c3df7
edson angel <edsonangelmc@gmail.com>
2024-05-17 19:06:49 -0600

    using cache headers in admin view

--- src/views/admin.php
+++ src/views/admin.php
@@ -1,5 +1,8 @@
 <?php
     $user_id = htmlspecialchars($_SESSION['ID']);
+    header("Cache-Control: no-cache, no-store, must-revalidate");
+    header("Pragma: no-cache");
+    header("Expires: 0");
 ?>
 <!DOCTYPE html>
 <html lang="en">
