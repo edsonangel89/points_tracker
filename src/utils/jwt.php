59307ccfa322cd400d72e16d4b3b8a12107b8452
edson angel <edsonangelmc@gmail.com>
2024-05-18 14:21:39 -0600

    test

--- src/utils/jwt.php
+++ src/utils/jwt.php
@@ -13,10 +13,10 @@
 
     function verify_jwt($jwt) {
         //echo 'jwt => ' . $jwt . "\n";
+        list($header_base64, $payload_base64, $signature_received_base64) = explode('.', $jwt);
         echo 'jwt => ' . $header_base64 . "\n";
         echo 'jwt => ' . $payload_base64 . "\n";
         echo 'jwt => ' . $signature_received_base64 . "\n";
-        list($header_base64, $payload_base64, $signature_received_base64) = explode('.', $jwt);
         $header = base64_encode($header_base64);
         $header_encoded = base64_decode($header);
         $payload = base64_encode($payload_base64);
