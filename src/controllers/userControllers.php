c516ab3803de1ced58b9483a36dfa40a3e0a16a9
edson angel <edsonangelmc@gmail.com>
2024-05-17 21:22:46 -0600

    testing user controllers

--- src/controllers/userControllers.php
+++ src/controllers/userControllers.php
@@ -50,8 +50,8 @@
             ];
             if($token_match) {
                 http_response_code(201);
-                header("Location: /mail?email=$email&token=$token");
-                //echo json_encode($user_info);
+                //header("Location: /mail?email=$email&token=$token");
+                echo json_encode($user_info);
             }
             else {
                 http_response_code(200);
