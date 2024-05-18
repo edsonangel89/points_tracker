d6c370b1eadf589134d16b24ba137db53ca6fbc4
edson angel <edsonangelmc@gmail.com>
2024-05-17 19:18:09 -0600

    added cache headers to info view

--- src/views/info.php
+++ src/views/info.php
@@ -1,4 +1,7 @@
 <?php
+    header("Cache-Control: no-cache, no-store, must-revalidate");
+    header("Pragma: no-cache");
+    header("Expires: 0");
     $user_id = htmlspecialchars($_SESSION['ID']);
 ?>
 <!DOCTYPE html>
