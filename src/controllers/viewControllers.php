b8be38d0142a1aef0cfa378c170c17c80c5e93f6
edson angel <edsonangelmc@gmail.com>
2024-05-19 14:49:44 -0600

    testing

--- src/controllers/viewControllers.php
+++ src/controllers/viewControllers.php
@@ -53,7 +53,7 @@
         //exit;
         $email_verified = $user['EmailVerified'];
         if(isset($_GET['token']) && !$email_verified) {
-            $token = htmlspecialchars(urlencode($_GET['token']));
+            $token = htmlspecialchars(urldecode($_GET['token']));
             $token_gen = urlencode(htmlspecialchars(generate_jwt($_GET['email'])));
             $token_match = verify_jwt($token);
             echo $token;
