04eaa70818c14e9586af2297ee67573a23bef38f
edson angel <edsonangelmc@gmail.com>
2024-05-19 20:00:18 -0600

    tet

--- src/utils/jwt.php
+++ src/utils/jwt.php
@@ -14,7 +14,7 @@
     function verify_jwt($jwt) {
         //echo 'jwt => ' . $jwt . "\n";
         list($header_base64, $payload_base64, $signature_received_base64) = explode('.', $jwt);
-        $signature_received_base64_urldec = urlencode($signature_received_base64);
+        $signature_received_base64_urldec = $signature_received_base64;
         //echo '<br>header_base64 => ' . $header_base64;
         //echo '<br>payload_base64 => ' . $payload_base64;
         //echo '<br>signature_received_base64 => ' . $signature_received_base64;
