76b8c1f6781d249e820729c1573d92d56996d68f
edson angel <edsonangelmc@gmail.com>
2024-05-20 01:17:14 -0600

    tet

--- src/controllers/userControllers.php
+++ src/controllers/userControllers.php
@@ -154,4 +154,21 @@
         }
     }
 
+    function get_jwt_verify($email) {
+        if(isset($_GET['email'])) {
+            $jwt = generate_jwt($email);
+            echo $jwt  . "<br>";
+            $verification = verify_jwt($jwt);
+            echo $verification  . "<br>";
+            exit;
+        }
+        else {
+            echo 'Error verification';
+        }
+    }
+
+    function jwt_sent() {
+
+    }
+
 ?>
