5e772a83ee5dee6a1afdef2957d782f0154fb600
edson angel <edsonangelmc@gmail.com>
2024-05-20 13:32:08 -0600

    removed english languege, added details

---
+++ src/views/privacy.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="src/public/css/home.css"/>
+    <title>Aviso de privacidad</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="nav-brand-container">
+                <a class="nav-brand-link" href="https://www.puntoaqua.com/"><img src="src/public/assets/brand/logo__svg.svg" alt="logo image"/></a>
+            </div>
+            <div class="nav-links-container-desktop">
+                <!--<a class="nav-link" href="#">Soporte</a>-->
+                <a class="nav-link" href="/home">Inicio</a>
+            </div>
+            <div class="nav-links-container-mobile-tablet">
+                <!--<a class="nav-link" href="#"><span class="material-symbols-outlined">support_agent</span></a>-->
+                <a class="nav-link" href="/home"><span class="material-symbols-outlined">home</span></a>
+            </div>
+        </nav>
+    </header>
+    
+</body>
+</html>
