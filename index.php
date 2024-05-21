2f47ec7596367165ad5e8f8a9e6758b5328a4430
edson angel <edsonangelmc@gmail.com>
2024-05-21 13:45:51 -0600

    fixed cookies expiration

--- index.php
+++ index.php
@@ -1,32 +1,12 @@
 <?php
     session_start();
 
-    if((isset($_COOKIE['Authorization']) && isset($_SESSION['ID'])) || (isset($_COOKIE['User']) && isset($_SESSION['ID']))) {
-        
-    }
-    else {
-        session_destroy();
-    }
-
-    /*require 'src/utils/jwt.php';
-    $test = generate_jwt('eascenciog65@gmail.com');
-    echo "JWT => " . $test . "\n";
-    verify_jwt($test);*/
-
-    
-    /*if ($_SESSION['ROLE'] == 'user') {
-        //call_user_func($views_routes[$path], 'user');
-        session_destroy();
-    }*/
-    /*if(isset($_SESSION['ID'])) {
-        if($_SESSION['ID'] == 1) {
-            if(!isset($_COOKIE['Authorization'])) {
-                session_destroy();
-                header('Location: /');
-            }
+    if(isset($_SESSION['ID'])) {
+        if(!isset($_COOKIE['Authorization']) || !isset($_COOKIE['User'])) {
+            session_destroy();
         }
-    }*/
-
+    }
+    
     require 'src/config/dbInit.php';
     require 'src/routes/index.php';
 ?>
