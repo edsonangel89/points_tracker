5a9b2dce45f6e4f954ebae17ea9e00d149f36664
edson angel <edsonangelmc@gmail.com>
2024-05-21 14:28:23 -0600

    adjusting cookie expiration

--- index.php
+++ index.php
@@ -5,6 +5,14 @@
         if(!isset($_COOKIE['Authorization']) && !isset($_COOKIE['User'])) {
             session_destroy();
         }
+        else {
+            if(isset($_COOKIE['Authorization'])) {
+                setcookie('Authorization', "$jwt", time() + 60,"/",false, true);
+            }
+            elseif(isset($_COOKIE['Authorization'])) {
+                setcookie('User', $_SESSION['ID'], time() + 60,"/",false, true);
+            }
+        }
     }
     
     require 'src/config/dbInit.php';
