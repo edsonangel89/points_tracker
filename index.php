ea4ce4e94d5d0f865ba02cf6068e7256a61b5430
edson angel <edsonangelmc@gmail.com>
2024-05-21 13:48:35 -0600

    fixed cookie expiration

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     session_start();
 
     if(isset($_SESSION['ID'])) {
-        if(!isset($_COOKIE['Authorization']) || !isset($_COOKIE['User'])) {
+        if(!isset($_COOKIE['Authorization']) && !isset($_COOKIE['User'])) {
             session_destroy();
         }
     }
