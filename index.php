2130f698274cf00190ad10a144fee285cb54a484
Ragnarok software <edsonangelmc@gmail.com>
2024-07-18 22:50:57 -0500

    test

--- index.php
+++ index.php
@@ -7,17 +7,17 @@
             header('Location: /');
         }
         else {
-            /*include 'src/utils/jwt.php';
+            include 'src/utils/jwt.php';
             $user_token = $_COOKIE['auth_token'];
             $decrypted_info = verify_jwt($user_token);
-            echo $decrypted_info;*/
+            echo $decrypted_info;
 
-            if(isset($_COOKIE['auth_token'])) {
-                setcookie('auth_token', $_COOKIE['auth_token'], time() + (3600 + 12),"/",false, true);
+            /*if(isset($_COOKIE['auth_token'])) {
+                setcookie('auth_token', $_COOKIE['auth_token'], time() + (3600 + 12),"/","",true, true);
             }
             elseif(isset($_COOKIE['User'])) {
                 setcookie('User', $_SESSION['ID'], time() + (86400 * 7),"/",false, true);
-            }
+            }*/
         }
     }
     
