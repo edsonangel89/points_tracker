0fdd3017610d12cfa562ece81aa29c70683acfb4
Ragnarok software <edsonangelmc@gmail.com>
2024-07-20 00:10:02 -0500

    test

--- src/utils/jwt.php
+++ src/utils/jwt.php
@@ -26,7 +26,9 @@
 
     function get_jwt_info($jwt) {
         list($header_base64, $payload_base64, $signature_received_base64) = explode('.', $jwt);
-        return base64_decode($payload_base64)['role'];
+        $payload_decoded = base64_decode($payload_base64); 
+        $role = $payload_decoded['role'];
+        return $role;
     }
 
 ?>
