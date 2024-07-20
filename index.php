1fc8179c73da19f90ecf392bffa1ef475c0f09cf
Ragnarok software <edsonangelmc@gmail.com>
2024-07-20 00:40:39 -0500

    test

--- index.php
+++ index.php
@@ -11,9 +11,9 @@
             $user_token = $_COOKIE['auth_token'];
             $user_role = get_jwt_info($user_token);
             if($user_role == 'admin' || $user_role == 'superadmin') {
-                echo "admin";
-            exit;
-                setcookie('auth_token', "$user_token", time() + (3600 + 12),"/","",true, true);
+                /*echo "admin";
+            exit;*/
+                setcookie('auth_token', "$user_token", time() + (3600 * 12),"/","",true, true);
             }
             elseif ($user_role == 'user') {
                 setcookie('auth_token', "$user_token", time() + (86400 * 7),"/","",true, true);
