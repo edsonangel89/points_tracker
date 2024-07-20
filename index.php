d64d320486b4f35ae3145955cb476246320fe304
Ragnarok software <edsonangelmc@gmail.com>
2024-07-20 00:36:12 -0500

    Modified auth_token

--- index.php
+++ index.php
@@ -9,9 +9,18 @@
         else {
             include 'src/utils/jwt.php';
             $user_token = $_COOKIE['auth_token'];
-            $is_token_correct = get_jwt_info($user_token);
-            echo $is_token_correct;
+            $user_role = get_jwt_info($user_token);
+            if($user_role == 'admin' || $user_role == 'superadmin') {
+                setcookie('auth_token', "$user_token", time() + (3600 + 12),"/","",true, true);
+            }
+            elseif ($user_role == 'user') {
+                setcookie('auth_token', "$user_token", time() + (86400 * 7),"/","",true, true);
+            }
+
+            /*
+            echo $user_role;
             exit;
+            */
             /*if(isset($_COOKIE['auth_token'])) {
                 setcookie('auth_token', $_COOKIE['auth_token'], time() + (3600 + 12),"/","",true, true);
             }
