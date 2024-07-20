226df1d656a2e96550f987ff4cfd01d06b5609fa
Ragnarok software <edsonangelmc@gmail.com>
2024-07-20 00:23:26 -0500

    test

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
             include 'src/utils/jwt.php';
             $user_token = $_COOKIE['auth_token'];
             $is_token_correct = get_jwt_info($user_token);
-            echo $is_token_correct['role'];
+            echo $is_token_correct;
             exit;
             /*if(isset($_COOKIE['auth_token'])) {
                 setcookie('auth_token', $_COOKIE['auth_token'], time() + (3600 + 12),"/","",true, true);
