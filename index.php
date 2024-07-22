7fb189ff97b35bf8c86ab8ae680591f2df4b349a
Ragnarok software <edsonangelmc@gmail.com>
2024-07-21 19:44:52 -0500

    tst

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
     require 'src/utils/jwt.php';
 
-    ini_set('session.use_cookies', 1);
-    ini_set('session.use_only_cookies', 1);
+    ini_set('session.use_cookies', 0);
+    ini_set('session.use_only_cookies', 0);
     ini_set('session.use_trans_sid', 0);
 
     session_start();
