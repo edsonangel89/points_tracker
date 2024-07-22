ea12c89f39a39b3e0c94e3c33deaec13345d803d
Ragnarok software <edsonangelmc@gmail.com>
2024-07-21 20:53:36 -0500

    tst

--- index.php
+++ index.php
@@ -12,33 +12,33 @@
             session_destroy();
             header('Location: /');
         }
-        else {
+        /*else {
             $user_token = $_COOKIE['auth_token'];
             $user_role = get_jwt_info($user_token);
             if($user_role == 'admin' || $user_role == 'superadmin') {
-                /*header("Authorization: Bearer $user_token");*/
+                header("Authorization: Bearer $user_token");
                 /*setcookie('auth_token', "$user_token", time() + (3600 * 12),"/","",true, true);
                 /*echo "admin";
-            exit;*/
+            exit;
             }
             elseif ($user_role == 'user') {
                 /*header("Authorization: Bearer $user_token");*/
                 /*setcookie('auth_token', "$user_token", time() + (86400 * 7),"/","",true, true);*/
                 /*echo "user";
-                exit;*/
+                exit;
             }
 
             /*
             echo $user_role;
             exit;
-            */
+            
             /*if(isset($_COOKIE['auth_token'])) {
                 setcookie('auth_token', $_COOKIE['auth_token'], time() + (3600 + 12),"/","",true, true);
             }
             elseif(isset($_COOKIE['User'])) {
                 setcookie('User', $_SESSION['ID'], time() + (86400 * 7),"/",false, true);
-            }*/
-        }
+            }
+        }*/
     }
     require 'src/config/dbInit.php';
     require 'src/routes/index.php';
