e77df45e2b7876019caa8d3862fc92a0065c5f36
Ragnarok software <edsonangelmc@gmail.com>
2024-07-21 19:02:33 -0500

    test

--- src/controllers/sessionControllers.php
+++ src/controllers/sessionControllers.php
@@ -31,8 +31,8 @@
                             ];
                             $jwt = generate_jwt($user_info);
                             $crypted_jwt = encrypt_jwt($jwt);
-                            header("Authorization: Bearer $crypted_jwt");
-                            /*setcookie('auth_token', "$crypted_jwt", time() + 43200,"/","",true, true);*/
+                            /*header("Authorization: Bearer $crypted_jwt");*/
+                            setcookie('auth_token', "$crypted_jwt", time() + 43200,"/","",true, true);
                             echo json_encode($user);
                         }
                         elseif ($_SESSION['ROLE'] == 'user'){
