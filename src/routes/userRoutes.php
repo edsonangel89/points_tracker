26fd526b62166a922345ae28abfba0ac8cad8d8b
Ragnarok software <edsonangelmc@gmail.com>
2024-07-24 22:06:11 -0500

    test

--- src/routes/userRoutes.php
+++ src/routes/userRoutes.php
@@ -39,13 +39,15 @@
             }
         }
         else {
-            $headers = getallheaders();
+            $headers = get_header("Authorization");
+            echo json_encode($headers);
+            exit;
+
             if(isset($_SESSION['ID']) && isset($_SESSION['ROLE']) || isset($headers['Authorization'])) {
                 
                 $token = substr($headers['Authorization'], 8);
                 $is_token_correct = verify_jwt_token($token);
-                echo json_encode($headers['Authorization']);
-                exit;
+                
                 
                 if($_SESSION['ID'] == $user_id || $_SESSION['ROLE'] == 'admin' || $_SESSION['ROLE'] == 'superadmin' || $is_token_correct) {
                     call_user_func($user_routes['/get/id'], $user_id);
