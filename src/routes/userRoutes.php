161f88540673e8a451ab70dcd36d43e601b7674d
Ragnarok software <edsonangelmc@gmail.com>
2024-07-24 21:13:41 -0500

    Modified authorization header access

--- src/routes/userRoutes.php
+++ src/routes/userRoutes.php
@@ -39,12 +39,12 @@
             }
         }
         else {
-            if(isset($_SESSION['ID']) && isset($_SESSION['ROLE'])) {
-                $headers = getallheaders();
-                if(isset($headers['Authorization'])) {
-                    $token = substr($headers['Authorization'], 8);
-                    $is_token_correct = verify_jwt_token($token);
-                }
+            $headers = getallheaders();
+            if(isset($_SESSION['ID']) && isset($_SESSION['ROLE']) || isset($headers['Authorization'])) {
+                
+                $token = substr($headers['Authorization'], 8);
+                $is_token_correct = verify_jwt_token($token);
+                
                 if($_SESSION['ID'] == $user_id || $_SESSION['ROLE'] == 'admin' || $_SESSION['ROLE'] == 'superadmin' || $is_token_correct) {
                     call_user_func($user_routes['/get/id'], $user_id);
                 }
