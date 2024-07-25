dc971f1a3a708b361672a0816e93b5e84e47b9d5
Ragnarok software <edsonangelmc@gmail.com>
2024-07-24 22:21:58 -0500

    test

--- src/routes/userRoutes.php
+++ src/routes/userRoutes.php
@@ -39,7 +39,7 @@
             }
         }
         else {
-            $headers = json_decode(getallheaders());
+            $headers = getallheaders();
             echo json_encode($headers);
             exit;
 
