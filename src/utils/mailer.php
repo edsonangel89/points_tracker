082c723b70d3df717fc8feb8ae3153f2a680104a
Ragnarok software <edsonangelmc@gmail.com>
2024-08-06 21:58:02 -0500

    test

--- src/utils/mailer.php
+++ src/utils/mailer.php
@@ -17,7 +17,7 @@
             $mail->Host       = 'smtp.gmail.com';                     
             $mail->SMTPAuth   = true;                                   
             $mail->Username   = 'puntoaquaoficial@gmail.com';                     
-            $mail->Password   = 'vbkysnwrtngkczzx';                               
+            $mail->Password   = 'puntoaquapointstracker2024';  /*vbkysnwrtngkczzx*/                             
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            
             $mail->Port       = 465; 
 
